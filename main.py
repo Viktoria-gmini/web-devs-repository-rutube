650b7a44edb71b9f4fb817538cbeca34ab49c19e
MrMarvel <seregakkk999@yandex.ru>
2023-10-28 03:21:23 +0300

    Made simple send data to fake rabbit

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import json
+from typing import Annotated
+
 from fastapi import FastAPI
-from fastapi import Request, Form
+from fastapi import Request, Form, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette import status
+from starlette.responses import JSONResponse
+
+from rabbit_wrapper import NNInterface, NNException
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+nn_interface = NNInterface.make_rabbitmq()
+
 
 # Function to process the input data and return an array of strings
 def process_input(article: str, text: str):
@@ -13,9 +23,11 @@ def process_input(article: str, text: str):
     text_array = text.split()
     return text_array
 
+
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post('/', response_class=HTMLResponse)
 def process_form(request: Request, article: str = Form(...), text: str = Form(...)):
@@ -23,6 +35,21 @@ def process_form(request: Request, article: str = Form(...), text: str = Form(..
     result = process_input(article, text)
     return templates.TemplateResponse("index.html", {"request": request, "result": result})
 
+@app.post('/api/process_data')
+def process_data(data = Form(...)):
+    data_json = dict()
+    try:
+        data_json = dict(json.loads(data))
+    except Exception as _:
+        return JSONResponse({"error": "Invalid JSON"}, status_code=status.HTTP_400_BAD_REQUEST)
+    try:
+        result = nn_interface.post(data_json)
+    except NNException as e:
+        return JSONResponse({"error": repr(e)}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return JSONResponse(result)
+
+
 if __name__ == '__main__':
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- rabbit_wrapper.py
+++ rabbit_wrapper.py
@@ -2,7 +2,7 @@ import json
 import threading
 import time
 import uuid
-from typing import Callable, Iterable, Mapping
+from abc import ABC, abstractmethod
 
 import pika
 from pika.frame import Method
@@ -10,29 +10,63 @@ from pika.frame import Method
 from thread_utils import ThreadController
 
 NN_INPUT_QUEUE_NAME = 'nn-input'
+MAX_NN_WAITING_TIME_SECONDS = 30
 
 
-class RabbitMQClient:
+class NNInterface(ABC):
+
+    @abstractmethod
+    def post(self, data: dict) -> dict:
+        raise NotImplementedError
+
+    @staticmethod
+    def make_rabbitmq() -> 'NNInterface':
+        return RabbitMQClient()
+
+
+class NNException(Exception):
+    pass
+class NNFailTimeoutException(NNException):
+    pass
+class NNReturnedBadJSONException(NNException):
+    pass
+
+
+class RabbitMQClient(NNInterface):
+    def post(self, data: dict) -> dict:
+        self._send_data(data)
+        if not self._result_event.wait(MAX_NN_WAITING_TIME_SECONDS):
+            raise NNFailTimeoutException(f"NN failed to respond in {MAX_NN_WAITING_TIME_SECONDS} seconds")
+        self._result_event.clear()
+        return self.response
+
     def __init__(self):
+        self._result_event = threading.Event()
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel = self.connection.channel()
         callback_result: Method = self.channel.queue_declare(queue='', exclusive=True)
         self._callback_queue_name: str = callback_result.method.queue
         self.channel.basic_consume(
             queue=self._callback_queue_name,
-            on_message_callback=self.on_response,
+            on_message_callback=self._on_response,
             auto_ack=True)
 
         self.response = None
         self.corr_id = None
         pass
 
-    def on_response(self, ch, method, props, body):
+    def _on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
-            self.response = body
+            try:
+                response_str = bytes.decode(body, encoding='utf-8')
+                self.response = dict(json.loads(response_str))
+                self._result_event.set()
+            except Exception as e:
+                print(e)
+                raise NNReturnedBadJSONException(f"NN returned bad data: {body}")
         pass
 
-    def send_data(self, data: dict):
+    def _send_data(self, data: dict):
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
                                    routing_key=NN_INPUT_QUEUE_NAME,
@@ -51,22 +85,22 @@ class RabbitMQServer:
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=NN_INPUT_QUEUE_NAME)
-        self.channel.basic_consume(queue=NN_INPUT_QUEUE_NAME, on_message_callback=self.on_request, auto_ack=True)
+        self.channel.basic_consume(queue=NN_INPUT_QUEUE_NAME, on_message_callback=self._on_request, auto_ack=False)
 
     def start(self):
         self.channel.start_consuming()
 
-    def on_request(self, ch, method: pika.spec.Basic.Deliver, props: pika.BasicProperties, body):
+    @staticmethod
+    def _on_request(ch, method: pika.spec.Basic.Deliver, props: pika.BasicProperties, body):
         data = json.loads(body)
         print(data)
-        result = f"Hi there! Sent data has length as str: {len(body)}"
+        result = {"result":f"Hi there! Sent data has length as str: {len(body)}"}
         ch.basic_publish(exchange='',
                          routing_key=props.reply_to,
                          properties=pika.BasicProperties(correlation_id=props.correlation_id),
-                         body=str(result))
-        # ch.basic_ack(delivery_tag=method.delivery_tag)
+                         body=str(json.dumps(result)))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
         pass
-
 
 
 def main():
@@ -79,9 +113,11 @@ def main():
         while True:
             rc.send_data(data={"text": "hello!"})
             time.sleep(1)
+            pass
+
     tc = ThreadController()
     tc.add_thread(run_server, name="Rabbit Server")
-    tc.add_thread(run_client, name="Rabbit Client")
+    # tc.add_thread(run_client, name="Rabbit Client")
     tc.run_infinite(block=True)
 
 
