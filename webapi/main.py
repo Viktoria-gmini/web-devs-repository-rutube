64e48aeefd22d7bcf04d739fbf07078946083168
MrMarvel <seregakkk999@yandex.ru>
2023-10-29 06:28:14 +0300

    Fixed sending to model

--- webapi/main.py
+++ webapi/main.py
@@ -80,7 +80,7 @@ def process_form(request: Request, article: str = Form(...), text: str = Form(..
 def process_data(data: str = Form(...)):
     data_json = dict()
     try:
-        data_json['text'] = str(data_json)
+        data_json['text'] = str(data)
     except Exception as _:
         return JSONResponse(content={"error": "Invalid JSON"}, status_code=status.HTTP_400_BAD_REQUEST)
     try:
